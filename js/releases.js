d5c04f97d97e7e83f088ea70aa981ad1f49dd0c5
Matheus Paiva <matheus.a.paiva@gmail.com>
2016-02-10 21:49:50 -0200

    Created releases page

---
+++ js/releases.js
@@ -0,0 +1,61 @@
+(function () {
+
+  // Get the releases list using the Github api
+  function getReleasesList() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://api.github.com/repos/gitpie/GitPie/releases', true);
+
+    xhr.onload = function () {
+      if (this.status == 200) {
+        var releaseList = JSON.parse(this.responseText);
+        var releasesContainer = document.querySelector('.releases-container');
+
+        for (var i = 0; i < releaseList.length; i++) {
+          var releaseDiv = document.createElement('div'),
+            innerHTML;
+
+          releaseDiv.className = 'release';
+
+          innerHTML = [
+            '<div class="release">',
+              '<h2>',
+                releaseList[i].tag_name,
+                '<span class="date">(', new Date(releaseList[i].created_at).toLocaleString(),')</span>',
+              '</h2>',
+              '<p class="title">',
+                releaseList[i].name,
+              '</p>',
+              '<p class="body">',
+                releaseList[i].body,
+              '</p>',
+              '<p>',
+                '<h3>Assets</h3>',
+                '<ul>',
+          ];
+
+          for (var j = 0; j < releaseList[i].assets.length; j++) {
+
+            innerHTML.push(
+              [
+                '<li>',
+                  '<a href="', releaseList[i].assets[j].browser_download_url,'">', releaseList[i].assets[j].name,'</a>',
+                '</li>'
+              ].join('')
+            );
+          }
+
+          innerHTML.push('</ul></p></div>');
+
+          releaseDiv.innerHTML = innerHTML.join('');
+
+          releasesContainer.appendChild(releaseDiv);
+        }
+      }
+    };
+
+    xhr.send();
+  }
+
+  getReleasesList();
+})();
